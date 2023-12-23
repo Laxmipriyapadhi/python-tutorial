638572fa179c645eb8f282038037a804b1cc7bb4
pinky <154205116+Laxmipriyapadhi@users.noreply.github.com>
2023-12-23 18:03:22 +0530

    Create code.py

---
+++ day3/code.py
@@ -0,0 +1,10 @@
+a=[]
+n=int(input("number of elements :"))
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            b=input()
+            a.append(b)
+                 
+print(a)
+                 
