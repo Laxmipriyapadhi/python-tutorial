78680375832292ae7b3302ed725c6de302ac6d38
pinky <154205116+Laxmipriyapadhi@users.noreply.github.com>
2024-01-10 19:32:44 +0530

    Update typeError.py

--- Day7/typeError.py
+++ Day7/typeError.py
@@ -1,3 +1,4 @@
+
 class Item:
     def __init__(self, name, type, others):
         self.name = name
@@ -9,38 +10,39 @@ class Item:
         self.size = len(self.name) + len(self.type)
 
 class Storage:
-    def __init__(self, host, port, protocol,max_size=10):
+    def __init__(self, host, port, protocol,max_size):
         self.host = host
         self.port = port
         self.protocol = protocol
         self.container = []
-        self.max_size = 10
-        self.current_size =7
+        self.max_size = max_size
+        
 
     def add_element(self, item):
-       if not isinstance(item,Item):
-           raise TypeERROR("invalid type of object")
-try:
-       if self.current_size + item.size > self.max_size:
-         raise MemoryERROR(" max size exceed")
-       self.container.append(item)
-       self.current_size += item.size
+       if isinstance(item):
+           raise TypeError("invalid type of object")
+       self.data.append(item)
+        
+       return item
 
-except:
+       
     def calculate_size(self):
-        return sum(item.size for item in self.container)
-try:    
-      storage = Storage(host="ghjhhb",port=555,protocol="https",max_size=10)
-     
-      item1 = Item("abc","type1",{"age":24})
-      storage.add_element(item1)
-      item2 = Item("xyz","type2",{"age":22})
-      storage.add_element(item2)
-      print("Max_size:",storage.max_size)
-      print("Current size:",storage.current_size)
-      print("New item size:",item2.size)
-except MemoryERROR as x:
-    print("Found error:{x}")
+        self.size = sum([item.size for item in self.container])
+        if self.size>self.max_size:
+            raise memoryError
+        return self.size
+try:        
+    storage = ("hjn",101,"http",10)
     
-finally:
-    print("Execution completed")
+    item1 = Item("abc","type1",{"age":24})
+    item2 = Item("xyz","type2",{"age":22})
+    
+    storage.add_element(item1)
+    storage.add_element(item2)
+    
+    print(storage.add_element(item2))
+    print("max_size:",storage.max_size)
+    print(storage.calculate_size())
+except (TypeError,MemoryError ):
+    print(f"error")
+  
