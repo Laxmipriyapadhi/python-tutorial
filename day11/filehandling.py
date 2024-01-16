a393b92ee98abc4698016a39294225ce4be6a1ee
pinky <154205116+Laxmipriyapadhi@users.noreply.github.com>
2024-01-16 17:38:12 +0530

    filehandling.py

--- day11/filehandling.py
+++ day11/filehandling.py
@@ -1,4 +1,4 @@
-## Create and write something in a file and then read the file and print the content, using with statement.
+
 
 with open ("log.txt", "w")as file:
         file.write("hello,this is log text.")
